8a0ee35e91f8779ee0df3ac879ef0fbf11cdce7c
Stefano Bozzano <bste02@gmail.com>
2024-05-04 12:55:54 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include "CRectangle.h"
+
+using namespace std;
+
+int main() {
+
+	Rectangle rectA(10,5);
+	Rectangle rectB(3,7);
+	
+	cout << endl;
+	cout << "****** rectangle A dump ******" << endl;
+	rectA.Dump(); 
+	cout << "****** ********* ******" << endl;
+	
+	cout << endl;
+	cout << "****** rectangle B dump ******" << endl;
+	rectB.Dump(); 
+	cout << "****** ********* ******" << endl;
+	
+	rectA.SetWidth(9);
+		
+	cout << endl;
+	cout << "****** rectangle A dump ******" << endl;
+	rectA.Dump();
+	cout << "****** ********* ******" << endl;
+	
+	cout << "Perimeter A : " << rectA.GetPerimeter() << endl; 
+	cout << "Area A : " << rectA.GetArea() << endl; 
+	cout << "Perimeter B : " << rectB.GetPerimeter() << endl; 
+	cout << "Area B : " << rectB.GetArea() << endl;
+	
+	return 0;
+
+}
